59fa3b2a41df900fc7b88c826847da14aa72a0ad
Nimira43 <n.rabone72@aol.com>
2026-05-15 12:21:46 +0100

    Created MessageBox component.

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -1,21 +1,39 @@
 import CardInnerWrapper from '@/components/CardInnerWrapper'
 import ChatForm from './ChatForm'
 import { getMessageThread } from '@/app/actions/messageActions'
+import MessageBox from './MessageBox'
+import { getAuthUserId } from '@/app/actions/authActions'
 
 export default async function ChatPage({
   params
 }: {
   params: {userId: string}
 }) {
+  const userId = await getAuthUserId()
   const messages = await getMessageThread(params.userId)
-  console.log({messages})
+  const body = (
+    <div>
+      {messages.length === 0
+        ? 'No messages to display'
+        : (
+          <div>
+            {messages.map(message => (
+              <MessageBox
+                key={message.id}
+                message={message}
+                currentUserId={userId}
+              />
+            ))}
+          </div>
+        )
+      }
+    </div>
+  )
   
   return (
     <CardInnerWrapper
       header='Chat'
-      body={
-        <div>Chat Placeholder</div>
-      }
+      body={body}
       footer={
         <ChatForm /> 
       }
